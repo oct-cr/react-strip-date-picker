adc0b35d1c15525c9e82ad97ade0cef36eac69bd
Octavio Coria <octavioxx@gmail.com>
2018-08-14 15:51:47 +0200

    disable Previous Week Control following minDate Limit

--- src/WeekStripDatePicker.js
+++ src/WeekStripDatePicker.js
@@ -2,7 +2,7 @@ import moment from 'moment'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { getWeekDays } from './utils'
+import { getWeekDays, getWeekMonday } from './utils'
 import { WeekStripDay } from './WeekStripDay'
 
 import styles from './common.css'
@@ -45,6 +45,10 @@ export class WeekStripDatePicker extends React.Component {
     const date = moment(this.state.visibleWeek)
     date.add(offsetDays, 'days')
 
+    if (this.props.minDate && (date.toDate() < this.getMinDate())) {
+      return
+    }
+
     this.setState(() => ({
       visibleWeek: date
     }))
@@ -57,12 +61,17 @@ export class WeekStripDatePicker extends React.Component {
 
 
   isDisabled(date) {
-    return this.props.minDate && (date.toDate().getDate() < this.props.minDate.getDate())
+    return this.props.minDate && (date.toDate() < this.getMinDate())
   }
 
 
-  render() {
-    const week = getWeekDays(this.state.visibleWeek).map(date => {
+  getMinDate() {
+    return new Date(this.props.minDate).setHours(0, 0, 0, 0)
+  }
+
+
+  renderWeekDays() {
+    return getWeekDays(this.state.visibleWeek).map(date => {
       const isDisabled = this.isDisabled(date)
 
       const wrapperProps = isDisabled ? {} :
@@ -77,17 +86,32 @@ export class WeekStripDatePicker extends React.Component {
         </div>
       )
     })
+  }
+
+
+  render() {
+    const isPreviousWeekDisabled = this.props.minDate &&
+      getWeekMonday(this.state.visibleWeek).toDate() <= this.getMinDate()
+
+    const wrapperProps = isPreviousWeekDisabled ?
+      {
+        style: { opacity: .5 }
+      } :
+      {
+        onClick: () => this.setVisibleWeek(-7),
+        style: { cursor: 'pointer' }
+      }
 
 
     return (
       <div className={styles.strip} >
-        <span onClick={e => this.setVisibleWeek(-7)}>
+        <span {...wrapperProps}>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
             <path d="M11.56 5.56L10.5 4.5 6 9l4.5 4.5 1.06-1.06L8.12 9z" />
           </svg>
         </span>
-        {week}
-        <span onClick={e => this.setVisibleWeek(7)} >
+        {this.renderWeekDays()}
+        <span onClick={e => this.setVisibleWeek(7)} style={{ cursor: 'pointer' }}>
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
             <path d="M7.5 4.5L6.44 5.56 9.88 9l-3.44 3.44L7.5 13.5 12 9z" />
           </svg>
